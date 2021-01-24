4adcd207f90e6e0f64777a718407d477a820380b
Nico Rehwaldt <git_nikku@nixis.de>
2021-01-25 00:57:53 +0100

    chore: add wuffle app

---
+++ wuffle.config.js
@@ -0,0 +1,69 @@
+/**
+ * This defines board specific configuration.
+ *
+ * ### Name
+ *
+ * The name of your board, displayed in the board header.
+ *
+ *
+ * ### Columns
+ *
+ * Valid fields for columns are
+ *
+ *  * name       - unique name
+ *  * label      - label, if present on a GitHub issue column will be set
+ *  * closed     - if the column issues must be closed
+ *  * sorting    - true if column cards should be sorted
+ *                 incrementally based on links
+ *  * states     - a list of board states to map to this column
+ *
+ * The default column is the column that holds open issues without
+ * any label constraints (Inbox in the example below).
+ *
+ */
+module.exports = {
+  "name": "bpmn-io/tasks",
+  "columns": [
+    {
+      "name": "Inbox",
+      "label": null,
+      "sorting": true,
+      "collapsed": true
+    },
+    {
+      "name": "Backlog",
+      "label": "backlog",
+      "sorting": true
+    },
+    {
+      "name": "Ready",
+      "label": "ready",
+      "sorting": true
+    },
+    {
+      "name": "In Progress",
+      "label": "in progress",
+      "sorting": true
+    },
+    {
+      "name": "Needs Review",
+      "label": "needs review",
+      "sorting": true
+    },
+    {
+      "name": "Fixed Upstream",
+      "label": "fixed upstream"
+    },
+    {
+      "name": "Done",
+      "label": null,
+      "closed": true,
+      "collapsed": true
+    }
+  ],
+  "allowedOrgs": [
+    "bpmn-io",
+    "zeebe-io",
+    "camunda"
+  ]
+};
